1391ceb898b80445a7ba46b3f97feabfbf1db0ac
Pavanpola030 <se21ucse150@mahindrauniversity.edu.in>
2026-05-06 16:00:39 +0530

    Fix public IP allocation method

--- terraform/task1/main.tf
+++ terraform/task1/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_public_ip" "pip" {
   name                = "task1-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_network_interface" "nic" {
