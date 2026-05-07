587f51141483bffe96628e1239dd229cc7f0797e
Pavanpola030 <se21ucse150@mahindrauniversity.edu.in>
2026-05-07 11:03:26 +0530

    Configure remote backend for task2

--- terraform/task2/main.tf
+++ terraform/task2/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "pavantfstate23561"
+    container_name       = "tfstate"
+    key                  = "task2.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 }
