c04e93f825faaa1356fc26a72772d1ffa72da390
Pavanpola030 <se21ucse150@mahindrauniversity.edu.in>
2026-05-11 16:08:35 +0530

    Update Task 5 alerts using direct VM metrics

--- terraform/task5/main.tf
+++ terraform/task5/main.tf
@@ -41,20 +41,17 @@ resource "azurerm_network_security_group" "nsg" {
   name                = "task5-nsg"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-}
-
-resource "azurerm_network_security_rule" "ssh" {
-  name                        = "Allow-SSH"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "22"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = data.azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface" "nic" {
